dd8ad0fd4658a2e4b8972d5b09c5358ed4144440
Jason S <jschlessman@gmail.com>
2017-06-12 16:44:50 -0400

    Pixiedust enabled version of base-notebook

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ "x$JUPYTER_NOTEBOOK_PASSWORD" != "x" ]]; then
+    HASH=$(python -c "from notebook.auth import passwd; print(passwd('$JUPYTER_NOTEBOOK_PASSWORD'))")
+    echo "c.NotebookApp.password = u'$HASH'" >> /home/$NB_USER/.jupyter/jupyter_notebook_config.py
+fi
+
+export PYTHONPATH=$SPARK_HOME/python:$(echo $SPARK_HOME/python/lib/py4j-*-src.zip)
+#exec jupyter notebook /home/nbuser/pixiedust/notebooks
+exec jupyter notebook
